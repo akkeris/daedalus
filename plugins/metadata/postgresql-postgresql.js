d049fcf5d150605eafae05918353e923ed4e32f5
Trevor Linton <trevor.linton@gmail.com>
2020-04-20 15:23:25 -0600

    Swallow link errors

--- plugins/metadata/postgresql-postgresql.js
+++ plugins/metadata/postgresql-postgresql.js
@@ -18,10 +18,14 @@ async function writePostgresqlTablesFromDatabases(pgpool) {
 
   const tableType = (await pgpool.query('select "type" from metadata.node_types where name = \'postgresql/tables\'')).rows[0].type;
   await Promise.all(tables.map(async (table) => {
-    await pgpool.query('insert into metadata.nodes (node, name, type, definition) values ($1, $2, $3, $4) on conflict (node) do nothing',
-      [table.table, `${table.catalog}.${table.schema}.${table.name} ${table.is_view ? '(view)' : ''}`, tableType, { ddl: table.definition }]);
-    await pgpool.query('insert into metadata.families (connection, parent, child) values (uuid_generate_v4(), $1, $2) on conflict (parent, child) do nothing',
-      [table.database, table.table]);
+    try {
+      await pgpool.query('insert into metadata.nodes (node, name, type, definition) values ($1, $2, $3, $4) on conflict (node) do nothing',
+        [table.table, `${table.catalog}.${table.schema}.${table.name} ${table.is_view ? '(view)' : ''}`, tableType, { ddl: table.definition }]);
+      await pgpool.query('insert into metadata.families (connection, parent, child) values (uuid_generate_v4(), $1, $2) on conflict (parent, child) do nothing',
+        [table.database, table.table]);
+    } catch (e) {
+      debug(`Unable to link table ${table.table} to database ${table.database} due to: ${e.message}`);
+    }
   }));
   await pgpool.query('delete from only metadata.nodes where nodes."type" = $1 and nodes.node not in (select "table" from postgresql.tables)', [tableType]);
 }
@@ -47,10 +51,14 @@ async function writePostgresqlColumnsFromTables(pgpool) {
   const columnType = (await pgpool.query('select "type" from metadata.node_types where name = \'postgresql/columns\'')).rows[0].type;
 
   await Promise.all(columns.map(async (column) => {
-    await pgpool.query('insert into metadata.nodes (node, name, type, definition) values ($1, $2, $3, $4) on conflict (node) do nothing',
-      [column.column, `${column.catalog}.${column.schema}.${column.table_name}.${column.name}`, columnType, { type: column.definition }]);
-    await pgpool.query('insert into metadata.families (connection, parent, child) values (uuid_generate_v4(), $1, $2) on conflict (parent, child) do nothing',
-      [column.table, column.column]);
+    try {
+      await pgpool.query('insert into metadata.nodes (node, name, type, definition) values ($1, $2, $3, $4) on conflict (node) do nothing',
+        [column.column, `${column.catalog}.${column.schema}.${column.table_name}.${column.name}`, columnType, { type: column.definition }]);
+      await pgpool.query('insert into metadata.families (connection, parent, child) values (uuid_generate_v4(), $1, $2) on conflict (parent, child) do nothing',
+        [column.table, column.column]);
+    } catch (e) {
+      debug(`Unable to link column ${column.column} and table ${column.table} due to: ${e.message}`);
+    }
   }));
 
   await pgpool.query('delete from only metadata.nodes where nodes."type" = $1 and nodes.node not in (select "column" from postgresql.columns)', [columnType]);

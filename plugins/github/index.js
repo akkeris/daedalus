a037cdb23401e75b63f3892a20fdeb4979185db8
Trevor Linton <trevor.linton@gmail.com>
2020-06-02 08:53:56 -0600

    COBRA-3340 - crawler for github (wip)

--- plugins/github/index.js
+++ plugins/github/index.js
@@ -158,18 +158,26 @@ async function run(pgpool) {
       } = await fetch(org, name); // eslint-disable-line no-await-in-loop
       await crawler.writeObj(pgpool, 'github', 'repo', repoNode(repo), repo, repoSpec(repo), repoStatus(repo), repoMetadata(repo), { url: repo.html_url, name: repo.full_name }); // eslint-disable-line max-len,no-await-in-loop
       for (const commit of commits) { // eslint-disable-line no-restricted-syntax
-        await crawler.writeObj(pgpool, 'github', 'commit', commitNode(commit), commit, commitSpec(commit), commitStatus(commit), commitMetadata(commit), { // eslint-disable-line max-len,no-await-in-loop
+        const columns = { // eslint-disable-line max-len,no-await-in-loop
           url: commit.html_url, sha: commit.sha, message: commit.commit.message, author: commit.commit.author, committer: commit.commit.committer, // eslint-disable-line max-len
-        }, { repo: { url: repo.html_url } });
+        };
+        const references = { repo: { url: repo.html_url } };
+        await crawler.writeObj(pgpool, 'github', 'commit', commitNode(commit), commit, commitSpec(commit), commitStatus(commit), commitMetadata(commit), columns, references); // eslint-disable-line max-len,no-await-in-loop
       }
       for (const branch of branches) { // eslint-disable-line no-restricted-syntax
-        await crawler.writeObj(pgpool, 'github', 'branch', branchNode(repo, branch), branch, branchSpec(branch), {}, branchMetadata(branch), { name: branch.name }, { repo: { url: repo.html_url } }); // eslint-disable-line max-len,no-await-in-loop
+        const columns = { name: branch.name };
+        const references = { repo: { url: repo.html_url } };
+        await crawler.writeObj(pgpool, 'github', 'branch', branchNode(repo, branch), branch, branchSpec(branch), {}, branchMetadata(branch), columns, references); // eslint-disable-line max-len,no-await-in-loop
       }
       for (const pull of pulls) { // eslint-disable-line no-restricted-syntax
-        await crawler.writeObj(pgpool, 'github', 'pull', pullNode(pull), pull, pullSpec(pull), pullStatus(pull), pullMetadata(pull), { url: pull.html_url, name: pull.title }, { repo: { url: repo.html_url } }); // eslint-disable-line max-len,no-await-in-loop
+        const columns = { url: pull.html_url, name: pull.title };
+        const references = { repo: { url: repo.html_url } };
+        await crawler.writeObj(pgpool, 'github', 'pull', pullNode(pull), pull, pullSpec(pull), pullStatus(pull), pullMetadata(pull), columns, references); // eslint-disable-line max-len,no-await-in-loop
       }
       for (const hook of hooks) { // eslint-disable-line no-restricted-syntax
-        await crawler.writeObj(pgpool, 'github', 'hook', hookNode(repo, hook), hook, hookSpec(hook), hookStatus(hook), hookMetadata(hook), { url: hook.html_url, name: hook.title }, { repo: { url: repo.html_url } }); // eslint-disable-line max-len,no-await-in-loop
+        const columns = { url: hook.html_url, name: hook.title };
+        const references = { repo: { url: repo.html_url } };
+        await crawler.writeObj(pgpool, 'github', 'hook', hookNode(repo, hook), hook, hookSpec(hook), hookStatus(hook), hookMetadata(hook), columns, references); // eslint-disable-line max-len,no-await-in-loop
       }
     } catch (e) {
       if (e.message.includes('code 404')) {
@@ -194,6 +202,7 @@ async function init(pgpool) {
   await crawler.createTableDefinition(pgpool, 'github', 'hook', { url: { type: 'text' }, name: { type: 'text' } }, ['repo']);
   debug('Initializing github plugin... done');
 }
+
 module.exports = {
   init,
   run,

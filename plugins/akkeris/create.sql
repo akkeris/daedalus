9890d24587d65cf3ebf9c75886e5217d6b932309
Trevor Linton <trevor.linton@gmail.com>
2020-06-04 21:17:02 -0600

    fix for tests

--- plugins/akkeris/create.sql
+++ plugins/akkeris/create.sql
@@ -3,16 +3,18 @@ begin
 
   create schema if not exists akkeris;
 
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/sites', 'akkeris.sites.svg', 'fa-sitemap', 'Akkeris Sites') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/routes', 'akkeris.routes.svg', 'fa-cube', 'Akkeris Routes') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/apps', 'akkeris.apps.svg', 'fa-microchip', 'Akkeris Apps') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/spaces', 'akkeris.spaces.svg', 'fa-cube', 'Akkeris Spaces') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addon_attachments', 'akkeris.addon_attachments.svg', 'fa-cube', 'Akkeris Addon Attachments') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addon_services', 'akkeris.addon_services.svg', 'fa-cube', 'Akkeris Services') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addons', 'akkeris.addons.svg', 'fa-cube', 'Akkeris Addons') on conflict (name) do nothing;
-  insert into metadata.node_types_fields (id, "type", jsonpath, name, friendly_name, format, highlighted) values 
-    (uuid_generate_v4(), (select "type" from metadata.node_types where name='akkeris/sites' limit 1), '$.region.name', 'region', 'Region', 'string', true) on conflict (type, name) do nothing;
-  
+
+  if exists (select 1 from information_schema.tables where table_schema='metadata' and table_name='node_types') then
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/sites', 'akkeris.sites.svg', 'fa-sitemap', 'Akkeris Sites') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/routes', 'akkeris.routes.svg', 'fa-cube', 'Akkeris Routes') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/apps', 'akkeris.apps.svg', 'fa-microchip', 'Akkeris Apps') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/spaces', 'akkeris.spaces.svg', 'fa-cube', 'Akkeris Spaces') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addon_attachments', 'akkeris.addon_attachments.svg', 'fa-cube', 'Akkeris Addon Attachments') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addon_services', 'akkeris.addon_services.svg', 'fa-cube', 'Akkeris Services') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'akkeris/addons', 'akkeris.addons.svg', 'fa-cube', 'Akkeris Addons') on conflict (name) do nothing;
+    insert into metadata.node_types_fields (id, "type", jsonpath, name, friendly_name, format, highlighted) values 
+      (uuid_generate_v4(), (select "type" from metadata.node_types where name='akkeris/sites' limit 1), '$.region.name', 'region', 'Region', 'string', true) on conflict (type, name) do nothing;
+  end if;
   -- dynamically created schemas
 
 end
--- plugins/aws/create.sql
+++ plugins/aws/create.sql
@@ -4,16 +4,18 @@ begin
   create schema if not exists "aws";
   create extension if not exists "uuid-ossp";
 
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/elastic_search', 'aws.es.svg', 'fa-search', 'Amazon Elastic Search') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_certificates', 'aws.rds_certificates.svg', 'fa-certificate', 'Amazon RDS Certificates') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_clusters', 'aws.rds_db_clusters.svg', 'fa-database', 'Amazon RDS Database Clusters') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_instances', 'aws.rds_db_instances.svg', 'fa-database', 'Amazon RDS Database Instances') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_parameter_groups', 'aws.rds_db_parameter_groups.svg', 'fa-cogs', 'Amazon Database Parameter Groups') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_security_groups', 'aws.rds_db_security_groups.svg', 'fa-shield', 'Amazon RDS Security Groups') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_snapshots', 'aws.rds_db_snapshots.svg', 'fa-archive', 'Amazon RDS Snapshots') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_subnet_groups', 'aws.rds_db_subnet_groups.svg', 'fa-plug', 'Amazon Subnet Groups') on conflict (name) do nothing;
-  insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_events', 'aws.rds_events.svg', 'fa-cube', 'Amazon RDS Events') on conflict (name) do nothing;
 
+  if exists (select 1 from information_schema.tables where table_schema='metadata' and table_name='node_types') then
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/elastic_search', 'aws.es.svg', 'fa-search', 'Amazon Elastic Search') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_certificates', 'aws.rds_certificates.svg', 'fa-certificate', 'Amazon RDS Certificates') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_clusters', 'aws.rds_db_clusters.svg', 'fa-database', 'Amazon RDS Database Clusters') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_instances', 'aws.rds_db_instances.svg', 'fa-database', 'Amazon RDS Database Instances') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_parameter_groups', 'aws.rds_db_parameter_groups.svg', 'fa-cogs', 'Amazon Database Parameter Groups') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_security_groups', 'aws.rds_db_security_groups.svg', 'fa-shield', 'Amazon RDS Security Groups') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_snapshots', 'aws.rds_db_snapshots.svg', 'fa-archive', 'Amazon RDS Snapshots') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_db_subnet_groups', 'aws.rds_db_subnet_groups.svg', 'fa-plug', 'Amazon Subnet Groups') on conflict (name) do nothing;
+    insert into metadata.node_types ("type", name, icon, fa_icon, human_name) values (uuid_generate_v4(), 'aws/rds_events', 'aws.rds_events.svg', 'fa-cube', 'Amazon RDS Events') on conflict (name) do nothing;
+  end if;
   -- this is dynamically created
 
 end
